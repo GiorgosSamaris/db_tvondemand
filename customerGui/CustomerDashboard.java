85bd86d16ce7b4cb1f187ab6f6e35672020af050
bgthelegend136 <bgthelegend@gmail.com>
2022-08-24 02:36:44 +0300

    Commite Updated

--- customerGui/CustomerDashboard.java
+++ customerGui/CustomerDashboard.java
@@ -13,6 +13,7 @@ import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.JButton;
 
 public class CustomerDashboard extends JFrame {
 	private Query qri = null;
@@ -71,34 +72,6 @@ public class CustomerDashboard extends JFrame {
 		ceidlogo.setBounds(10, 11, 74, 39);
 		panel.add(ceidlogo);
 		
-		JLabel settingsdash = new JLabel("Settings");
-		settingsdash.setForeground(new Color(255, 255, 255));
-		settingsdash.setFont(new Font("Dialog", Font.BOLD, 15));
-		settingsdash.setHorizontalAlignment(SwingConstants.CENTER);
-		settingsdash.setBounds(74, 122, 208, 60);
-		panel.add(settingsdash);
-		
-		JLabel showmyrents = new JLabel("Show My Rents");
-		showmyrents.setForeground(new Color(255, 255, 255));
-		showmyrents.setFont(new Font("Dialog", Font.BOLD, 15));
-		showmyrents.setHorizontalAlignment(SwingConstants.CENTER);
-		showmyrents.setBounds(74, 183, 208, 60);
-		panel.add(showmyrents);
-		
-		JLabel cataloguedash = new JLabel("Catalogue");
-		cataloguedash.setForeground(new Color(255, 255, 255));
-		cataloguedash.setFont(new Font("Dialog", Font.BOLD, 15));
-		cataloguedash.setHorizontalAlignment(SwingConstants.CENTER);
-		cataloguedash.setBounds(74, 242, 208, 60);
-		panel.add(cataloguedash);
-		
-		JLabel signoutdash = new JLabel("Sign Out");
-		signoutdash.setForeground(new Color(255, 255, 255));
-		signoutdash.setFont(new Font("Dialog", Font.BOLD, 15));
-		signoutdash.setHorizontalAlignment(SwingConstants.CENTER);
-		signoutdash.setBounds(74, 307, 208, 60);
-		panel.add(signoutdash);
-		
 		JLabel logohome = new JLabel("HomeImg");
 		logohome.setBounds(10, 61, 48, 60);
 		panel.add(logohome);
@@ -119,21 +92,78 @@ public class CustomerDashboard extends JFrame {
 		signoutlogo.setBounds(10, 307, 48, 60);
 		panel.add(signoutlogo);
 		
-		JLabel homedash = new JLabel("Home");
-		homedash.setBounds(74, 61, 208, 60);
-		panel.add(homedash);
-		homedash.setForeground(new Color(255, 255, 255));
-		homedash.setFont(new Font("Dialog", Font.BOLD, 15));
-		homedash.setHorizontalAlignment(SwingConstants.CENTER);
-		
 		JLabel lblNewLabel = new JLabel("Welcome Email");
 		lblNewLabel.setFont(new Font("Serif", Font.BOLD, 11));
 		lblNewLabel.setBounds(114, 11, 128, 39);
 		panel.add(lblNewLabel);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(280, 0, 522, 378);
-		contentPane.add(panel_1);
+		JPanel homepanel = new JPanel();
+		homepanel.setBackground(new Color(0, 128, 128));
+		homepanel.setBounds(74, 60, 208, 61);
+		panel.add(homepanel);
+		homepanel.setLayout(null);
+		
+		JLabel Homelabel = new JLabel("Home");
+		Homelabel.setBackground(new Color(0, 128, 128));
+		Homelabel.setHorizontalAlignment(SwingConstants.CENTER);
+		Homelabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		Homelabel.setBounds(0, 0, 208, 61);
+		homepanel.add(Homelabel);
+		
+		JPanel settingspanel = new JPanel();
+		settingspanel.setBackground(new Color(0, 128, 128));
+		settingspanel.setBounds(74, 121, 208, 61);
+		panel.add(settingspanel);
+		settingspanel.setLayout(null);
+		
+		JLabel settingslabel = new JLabel("Settings");
+		settingslabel.setBackground(new Color(0, 128, 128));
+		settingslabel.setHorizontalAlignment(SwingConstants.CENTER);
+		settingslabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		settingslabel.setBounds(0, 0, 208, 61);
+		settingspanel.add(settingslabel);
+		
+		JPanel showmyrentpanel = new JPanel();
+		showmyrentpanel.setBackground(new Color(0, 128, 128));
+		showmyrentpanel.setBounds(74, 183, 208, 61);
+		panel.add(showmyrentpanel);
+		showmyrentpanel.setLayout(null);
+		
+		JLabel myrentslabel = new JLabel("My rents");
+		myrentslabel.setBackground(new Color(0, 128, 128));
+		myrentslabel.setHorizontalAlignment(SwingConstants.CENTER);
+		myrentslabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		myrentslabel.setBounds(0, 0, 208, 61);
+		showmyrentpanel.add(myrentslabel);
+		
+		JPanel CataloguePanel = new JPanel();
+		CataloguePanel.setBackground(new Color(0, 128, 128));
+		CataloguePanel.setBounds(74, 242, 208, 61);
+		panel.add(CataloguePanel);
+		CataloguePanel.setLayout(null);
+		
+		JLabel cataloguelabel = new JLabel("Catalogue");
+		cataloguelabel.setBackground(new Color(0, 128, 128));
+		cataloguelabel.setHorizontalAlignment(SwingConstants.CENTER);
+		cataloguelabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		cataloguelabel.setBounds(0, 0, 208, 61);
+		CataloguePanel.add(cataloguelabel);
+		
+		JPanel signoutpanel = new JPanel();
+		signoutpanel.setBackground(new Color(0, 128, 128));
+		signoutpanel.setLayout(null);
+		signoutpanel.setBounds(74, 306, 208, 61);
+		panel.add(signoutpanel);
+		
+		JLabel signoutlabel = new JLabel("Sign out");
+		signoutlabel.setBackground(new Color(0, 102, 102));
+		signoutlabel.setHorizontalAlignment(SwingConstants.CENTER);
+		signoutlabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		signoutlabel.setBounds(0, 0, 208, 61);
+		signoutpanel.add(signoutlabel);
+		
+		JPanel MainPanel = new JPanel();
+		MainPanel.setBounds(280, 0, 522, 378);
+		contentPane.add(MainPanel);
 	}
-
 }
