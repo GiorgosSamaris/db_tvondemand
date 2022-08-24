7dfe6e1d774753b6d14a67f5de81de04443b589a
GiorgosSamaris <georgelmt10219@gmail.com>
2022-08-24 23:31:19 +0300

    Idk

--- customerGui/PanelCatalogue.java
+++ customerGui/PanelCatalogue.java
@@ -4,20 +4,36 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JTabbedPane;
-
+import queryToDB.*;
+import content.*;
 public class PanelCatalogue extends JPanel {
 
+	
+	private static Query qri =null;
 	/**
 	 * Create the panel.
 	 */
-	public PanelCatalogue() {
+	public PanelCatalogue(Query query) {
+		qri = query;
 		setBackground(new Color(112, 128, 144));
 		setBounds(0, 0, 522,378);
 		setLayout(null);
 		
+		
+		JList<Film> list = new JList<Film>();
+		try
+		{
+			list = qri.getAvailableFilms();
+			
+		}
+		catch(Exception e)
+		{
+			
+		}
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 42, 502, 308);
 		add(scrollPane);
