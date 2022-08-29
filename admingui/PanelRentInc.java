266c2572337d77cecf90f946c1a76f9cc903b247
bgthelegend136 <bgthelegend@gmail.com>
2022-08-29 16:37:13 +0300

    RentsIncomeUpdate

--- admingui/PanelRentInc.java
+++ admingui/PanelRentInc.java
@@ -7,8 +7,12 @@ import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class PanelRentInc extends JPanel {
+	private JTable table_Series;
+	private JTable table_Films;
 
 	/**
 	 * Create the panel.
@@ -18,37 +22,62 @@ public class PanelRentInc extends JPanel {
 		setSize(593,503);
 		setLayout(null);
 		
-		JLabel lblNewLabel_2_1 = new JLabel("Month:");
-		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_2_1.setBounds(149, 66, 73, 27);
-		add(lblNewLabel_2_1);
+		JLabel lbMonth = new JLabel("Month:");
+		lbMonth.setHorizontalAlignment(SwingConstants.CENTER);
+		lbMonth.setFont(new Font("Serif", Font.BOLD, 13));
+		lbMonth.setBounds(132, 49, 73, 27);
+		add(lbMonth);
 		
-		JLabel lblNewLabel_1 = new JLabel("Select Date");
-		lblNewLabel_1.setToolTipText("This tool calculates the 5 most rented films/series based on the prior moth from the date you have inserted.");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Serif", Font.BOLD, 15));
-		lblNewLabel_1.setBounds(181, 11, 88, 27);
-		add(lblNewLabel_1);
+		JLabel SelectDate = new JLabel("Select Date");
+		SelectDate.setToolTipText("This tool calculates the 5 most rented films/series based on the prior moth from the date you have inserted.");
+		SelectDate.setHorizontalAlignment(SwingConstants.CENTER);
+		SelectDate.setFont(new Font("Serif", Font.BOLD, 15));
+		SelectDate.setBounds(181, 11, 88, 27);
+		add(SelectDate);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}));
-		comboBox_1.setMaximumRowCount(12);
-		comboBox_1.setBounds(229, 69, 79, 20);
-		add(comboBox_1);
+		JComboBox comboBox_month = new JComboBox();
+		comboBox_month.setModel(new DefaultComboBoxModel(new String[] {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}));
+		comboBox_month.setMaximumRowCount(12);
+		comboBox_month.setBounds(225, 53, 79, 20);
+		add(comboBox_month);
 		
-		JLabel lblNewLabel_2_2 = new JLabel("Year:");
-		lblNewLabel_2_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_2.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_2_2.setBounds(318, 66, 73, 27);
-		add(lblNewLabel_2_2);
+		JLabel LbYEAR = new JLabel("Year:");
+		LbYEAR.setHorizontalAlignment(SwingConstants.CENTER);
+		LbYEAR.setFont(new Font("Serif", Font.BOLD, 13));
+		LbYEAR.setBounds(314, 49, 73, 27);
+		add(LbYEAR);
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000"}));
-		comboBox_2.setBounds(384, 70, 60, 20);
-		add(comboBox_2);
+		JComboBox comboBox_year = new JComboBox();
+		comboBox_year.setModel(new DefaultComboBoxModel(new String[] {"2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000"}));
+		comboBox_year.setBounds(383, 53, 60, 20);
+		add(comboBox_year);
+		
+		JScrollPane scrollPane_Films = new JScrollPane();
+		scrollPane_Films.setBounds(23, 120, 548, 164);
+		add(scrollPane_Films);
+		
+		table_Films = new JTable();
+		scrollPane_Films.setViewportView(table_Films);
+		
+		JScrollPane scrollPane_Series = new JScrollPane();
+		scrollPane_Series.setBounds(23, 316, 548, 164);
+		add(scrollPane_Series);
+		
+		table_Series = new JTable();
+		scrollPane_Series.setViewportView(table_Series);
+		
+		JLabel LbSeries = new JLabel("Series");
+		LbSeries.setHorizontalAlignment(SwingConstants.CENTER);
+		LbSeries.setFont(new Font("Serif", Font.BOLD, 15));
+		LbSeries.setBounds(157, 291, 230, 14);
+		add(LbSeries);
+		
+		JLabel LbFilms = new JLabel("Films");
+		LbFilms.setHorizontalAlignment(SwingConstants.CENTER);
+		LbFilms.setFont(new Font("Serif", Font.BOLD, 15));
+		LbFilms.setBounds(142, 95, 230, 14);
+		add(LbFilms);
 
 
 	}
-
 }
